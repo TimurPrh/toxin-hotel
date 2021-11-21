90af6e6a01cd437957f72f1c89b93270bc99c7ef
Timur <timurprh@gmail.com>
2021-11-21 16:02:44 +0300

    Added adaptive for header and footer

--- src/pages/blocks/header/header.js
+++ src/pages/blocks/header/header.js
@@ -1,2 +1,11 @@
 import '../button/button.js';
 import './header.scss';
+
+document.querySelector('.header__hamburger').addEventListener('click', (e) => {
+    document.querySelector('.header__hamburger').classList.toggle('header__hamburger_active');
+    if (document.querySelector('.header__wrapper').style.left == "0%") {
+        document.querySelector('.header__wrapper').style.left = "-100%";
+    } else {
+        document.querySelector('.header__wrapper').style.left = "0%";
+    }
+});
